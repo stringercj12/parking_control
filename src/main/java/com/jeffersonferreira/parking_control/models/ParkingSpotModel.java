551afe1ed0a4fb3fb4d36409906512c53a67dd45
Jefferson Ferreira <jefferson14489@gmail.com>
2024-06-11 17:57:03 -0300

    Initial Commit

---
+++ src/main/java/com/jeffersonferreira/parking_control/models/ParkingSpotModel.java
@@ -0,0 +1,14 @@
+package com.jeffersonferreira.parking_control.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name =  "tb_parking_spot")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    
+}
